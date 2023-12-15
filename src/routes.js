7092bd5252848edc41fe804a2a45dbdc8dc508ce
samuel felix <samuelfnatal@hotmail.com>
2023-12-15 11:35:45 -0300

    commit inicial do meu servidor

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const routes = express.Router();
+const users = [
+  {
+    id: 1,
+    name: "Samuel",
+    email: "samuelfnatal@hotmail.com",
+    password: "12345",
+  },
+];
+
+routes.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    return res.status(200).json(user);
+  }
+  return res.status(401).json({ message: "Email ou Senha Inválida" });
+});
+
+module.exports = routes;
